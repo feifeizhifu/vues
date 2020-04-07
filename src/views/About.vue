8ccfae5f69f49f22a9e48ecac103d3f1fca1a1e9
Your Name <you@example.com>
2020-04-07 21:39:11 +0800

    11

---
+++ src/views/About.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="wrapper">
+        <div class="main">
+            <h2>标题ddd</h2>
+            <div class="title">
+                <p>1111</p>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+
+</style>
